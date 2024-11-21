5d1ae7d8970ee550af8a61f5f89c634807c58ff9
tadpoletrampoline <tihami.islam@student.tdsb.on.ca>
2024-11-21 13:36:06 -0500

    Update ComparisonResult.java
    sorted elements in the method

--- ComparisonResult.java
+++ ComparisonResult.java
@@ -3,22 +3,64 @@ import java.io.*;
 
 public class ComparisonResult{
     
-    private ArrayList<String> list;
+    private String name;
     
-    public ComparisonResult(ArrayList<String> list) {
-        this.list = list;
+    public ComparisonResult(String name) {
+        this.name = name;
     }
 
-    public sortElements() {
-
-        System.out.println("sort the the strings in the list so that each word/character seperated by a space is individualized");
+    public ArrayList<String> sortElements() {
         
-    }
+            // Get each line of file in a list
+    		ArrayList<String> lines = new ArrayList<>();
+    		
+    		try { //testing reading one file 
+    		    File myFile = new File(this.name);
+    		    Scanner scanner = new Scanner(myFile);
     
-    public compare(ComparisonResult list, ArrayList<String> ignores) {
+            if (myFile.exists()) {
+                    System.out.println("File name: " + myFile.getName());
+                    //System.out.println("File size in bytes " + myFile.length());
+                } 
+                else {
+                    System.out.println("The file does not exist.");
+                }
+                
+            if(!scanner.hasNext()==true){
+                    System.out.println("Error: File is empty");
+                }
+                
+            while(scanner.hasNextLine()) {
+                
+                String line = scanner.nextLine();
+                lines.add(line);
+            }
+            
+            scanner.close();
+    
+    		}
+    		catch (FileNotFoundException e) {
+                System.out.println("File not found: " + e.getMessage());
+            } catch (NumberFormatException e) {
+                System.out.println("Error parsing year: " + e.getMessage());
+            }
+            
+            System.out.println("The contents of the file are: " + lines + "\n\n");
+    		
+    		for (String line : lines) {
+    		    System.out.println(line);
+    		}
+    	   
+    	   return lines;
+
+		}
+		
+        // Seperate all the characters in the file into a list
+    
+    
+   /* public  compare(ComparisonResult list, ArrayList<String> ignores) {
 
         System.out.println("Compare the two elemenet lists (call the first method on the parameter list object), as well, don't add to sum if characters are in ignore list");
-    
-    }
+    } */
 
 }
