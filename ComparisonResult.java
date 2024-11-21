75f9ab0c3a0861ff54607d7b3cda2d3ed351ca8e
Unaizahh <unaizah.qutub@student.tdsb.on.ca>
2024-11-20 21:07:21 -0500

    Update ComparisonResult.java
    skeleton code of a few methods

--- ComparisonResult.java
+++ ComparisonResult.java
@@ -8,5 +8,17 @@ public class ComparisonResult{
     public ComparisonResult(ArrayList<String> list) {
         this.list = list;
     }
+
+    public sortElements() {
+
+        System.out.println("sort the the strings in the list so that each word/character seperated by a space is individualized");
+        
+    }
     
+    public compare(ComparisonResult list, ArrayList<String> ignores) {
+
+        System.out.println("Compare the two elemenet lists (call the first method on the parameter list object), as well, don't add to sum if characters are in ignore list");
+    
+    }
+
 }
