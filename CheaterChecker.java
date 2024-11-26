fb1923fcbe6eb176599fb7094a7db9fee788c27d
tadpoletrampoline <tihami.islam@student.tdsb.on.ca>
2024-11-25 20:23:16 -0500

    Update CheaterChecker.java

--- CheaterChecker.java
+++ CheaterChecker.java
@@ -5,6 +5,7 @@ import java.io.*;
 public class CheaterChecker
 {
 	static Scanner options = new Scanner(System.in);
+	static Scanner scanner = new Scanner(System.in);
 	
 	public static void main(String[] args) {
 		String filePath1 = "SumOfMultiples1.txt";
